0727b274640e1168762f3226a91d9a98291301b3
Zahan Malkani <zahanm@gmail.com>
2012-12-02 17:56:32 -0800

    bigger axes

--- genData.py
+++ genData.py
@@ -68,6 +68,7 @@ def graph(fname):
     xs[loc] = float(m.group(1))
     ys[loc] = float(m.group(3))
     cats[loc] = 3
+    plt.axes([-0.5, -0.5, np.max(xs) + 0.5, np.max(ys) + 0.5])
     plt.plot(xs[ cats == 1 ], ys[ cats == 1 ], "bo")
     plt.plot(xs[ cats == 2 ], ys[ cats == 2 ], "ro")
     plt.plot(xs[ cats == 3 ], ys[ cats == 3 ], "yo")
